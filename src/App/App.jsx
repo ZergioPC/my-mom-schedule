864a9eb9ba6b0b4d3e762f66aab4de2f6f6dfa75
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-01-11 20:09:34 -0500

    INIT. Creacion del proyecto y posibles rutas

---
+++ src/App/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <p>Matriz eisenhower</p>
+      <p>Plan General</p>
+      <p>Planificador semanal</p>
+    </>
+  )
+}
+
+export default App
