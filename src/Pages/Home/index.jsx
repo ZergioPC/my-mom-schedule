b981320b4950604d8c44916bdc66901de8f1529f
Sergio Palacios 2 <sergid26pal@gmail.com>
2026-01-17 17:44:14 -0500

    FEATURE. El home ahora muestra los cambios realizados del panel semanal y la matriz

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,10 +3,10 @@ import React from "react";
 
 import useLocalStorage from "../../Hooks/useLocalStorage";
 import useCronograma from "../../Hooks/useCronograma";
-import usePlanSemanal from "../../Hooks/usePlanSemanal";
 
 import idMatchToWeek from "../../Utils/idMatchToWeek";
-import useMatrizCtx from "../../Hooks/useMatrizCtx";
+
+import { useManageData, dayTypes, timeTypes, matrizTypes } from "../../Hooks/useManageData";
 
 const currentInit = {
     "id": 0,
@@ -15,13 +15,28 @@ const currentInit = {
     "tareas": [],
 }
 
+const days = [
+  { label: "Lunes", value: dayTypes.lunes },
+  { label: "Martes", value: dayTypes.martes },
+  { label: "Miércoles", value: dayTypes.miercoles },
+  { label: "Jueves", value: dayTypes.jueves },
+  { label: "Viernes", value: dayTypes.viernes },
+  { label: "Sábado", value: dayTypes.sabado },
+  { label: "Domingo", value: dayTypes.domingo },
+]
+
 function Home(){
+  const {tareas} = useManageData();
+
   const [startWeek, _ ] = useLocalStorage("Start","");
   const [currentWeekData, setCurrent ] = useLocalStorage("CurrentWeek", currentInit);
 
   const { cronograma } = useCronograma();
-  const { semanal } =  usePlanSemanal();
-  const { matriz } = useMatrizCtx();  
+
+  const urgenteItems = tareas.filter(tarea => tarea.matriz === matrizTypes.urgente);
+  const noUrgenteItems = tareas.filter(tarea => tarea.matriz === matrizTypes.noUrgente);
+  const importanteItems = tareas.filter(tarea => tarea.matriz === matrizTypes.importante);
+  const noImportanteItems = tareas.filter(tarea => tarea.matriz === matrizTypes.noImportante);
 
   React.useEffect(()=>{
     setCurrent(cronograma.find(item =>
@@ -68,57 +83,55 @@ function Home(){
           <h2>Pendientes de la semana</h2>
           
           <div className="Home-semana-container">
-            <article>
-              <h3>Hogar</h3>
-              <ol>
-                  {semanal.hogar.length === 0 ? (
-                    <p>No hay tareas de hogar aún...</p>
-                  ) : (
-                    semanal.hogar.map((item) => 
-                      <li key={item.id}>{item.txt}</li>
-                    )
-                  )}
-              </ol>
-            </article>
+            {days.map((day, idx) => (
+              <article key={idx}>
+                <h2>{day.label}</h2>
+                
+                <ul className="Home-semana-morning">
+                  {tareas.filter(item => 
+                    item.day === day.value && item.time === timeTypes.morning
+                  ).map((item,idx)=>(
+                    <li 
+                      key={idx}
+                      className={item.complete ? "Home-semana-check" : ""}
+                    >{item.txt}</li>
+                  ))}
+                </ul>
+                
+                <ul className="Home-semana-midday">
+                  {tareas.filter(item => 
+                    item.day === day.value && item.time === timeTypes.midday
+                  ).map((item,idx)=>(
+                    <li 
+                      key={idx}
+                      className={item.complete ? "Home-semana-check" : ""}
+                    >{item.txt}</li>
+                  ))}
+                </ul>
 
-            <article>
-              <h3>Estudio</h3>
-              <ol>
-                  {semanal.acaademia.length === 0 ? (
-                    <p>No hay tareas de estudio aún...</p>
-                  ) : (
-                    semanal.acaademia.map((item) => 
-                      <li key={item.id}>{item.txt}</li>
-                    )
-                  )}
-              </ol>
-            </article>
+                <ul className="Home-semana-afternoon">
+                  {tareas.filter(item => 
+                    item.day === day.value && item.time === timeTypes.afternoon
+                  ).map((item,idx)=>(
+                    <li 
+                      key={idx}
+                      className={item.complete ? "Home-semana-check" : ""}
+                    >{item.txt}</li>
+                  ))}
+                </ul>
 
-            <article>
-              <h3>Trabajo</h3>
-              <ol>
-                  {semanal.laboral.length === 0 ? (
-                    <p>No hay tareas de trabajo aún...</p>
-                  ) : (
-                    semanal.laboral.map((item) => 
-                      <li key={item.id}>{item.txt}</li>
-                    )
-                  )}
-              </ol>
-            </article>
-
-            <article>
-              <h3>Ocio</h3>
-              <ol>
-                  {semanal.ocio.length === 0 ? (
-                    <p>No hay tareas de ocio aún...</p>
-                  ) : (
-                    semanal.ocio.map((item) => 
-                      <li key={item.id}>{item.txt}</li>
-                    )
-                  )}
-              </ol>
-            </article>
+                <ul className="Home-semana-night">
+                  {tareas.filter(item => 
+                    item.day === day.value && item.time === timeTypes.night
+                  ).map((item,idx)=>(
+                    <li 
+                      key={idx}
+                      className={item.complete ? "Home-semana-check" : ""}
+                    >{item.txt}</li>
+                  ))}
+                </ul>
+              </article>
+            ))}
           </div>
         </section>
 
@@ -128,11 +141,11 @@ function Home(){
           <h2>Matriz Eisenhower</h2>
 
           <article className="matriz-urgente">
-            {matriz.urgente.length === 0 ? (
+            {urgenteItems.length === 0 ? (
               <p>No hay urgencias de mucha importancia</p>
             ) : (
             <ul>
-              {matriz.urgente.map((item,idx)=>(
+              {urgenteItems.map((item,idx)=>(
                 <li key={idx}>{item.txt}</li>
               ))}
             </ul>
@@ -140,11 +153,11 @@ function Home(){
           </article>
 
           <article className="matriz-importante">
-            {matriz.noUrgente.length === 0 ? (
+            {noUrgenteItems.length === 0 ? (
               <p>No hay urgencias de poca importancia</p>
             ) : (
             <ul>
-              {matriz.noUrgente.map((item,idx)=>(
+              {noUrgenteItems.map((item,idx)=>(
                 <li key={idx}>{item.txt}</li>
               ))}
             </ul>
@@ -152,11 +165,11 @@ function Home(){
           </article>
 
           <article className="matriz-nourgente">
-            {matriz.importante.length === 0 ? (
+            {importanteItems.length === 0 ? (
               <p>No hay tareas importantes no urgentes</p>
             ) : (
             <ul>
-              {matriz.importante.map((item,idx)=>(
+              {importanteItems.map((item,idx)=>(
                 <li key={idx}>{item.txt}</li>
               ))}
             </ul>
@@ -164,11 +177,11 @@ function Home(){
           </article>
 
           <article className="matriz-noimportante">
-            {matriz.noImportante.length === 0 ? (
+            {noImportanteItems.length === 0 ? (
               <p>No hay tareas ni urgentes ni importantes</p>
             ) : (
             <ul>
-              {matriz.noImportante.map((item,idx)=>(
+              {noImportanteItems.map((item,idx)=>(
                 <li key={idx}>{item.txt}</li>
               ))}
             </ul>
